00305d2ac2555905a7965f0061ed2e19208331ea
David Weatherstone <davidweatherstone@gmail.com>
2023-01-26 16:28:08 +0000

    Part 4: Adding proper return values, error handling and basic pagination.

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ The second part of the tutorial introduces the use of databases, specifically us
 
 The third part of the tutorial is [here](https://medium.com/better-programming/building-the-rust-web-app-how-to-use-object-relational-mapper-3af2084555b6), and adds Object Relational Mapping, and a docker container for the Postgres database, rather than setting it up locally.
 
-The fourth part of the tutorial is [here](https://medium.com/better-programming/building-the-rust-web-app-proper-error-handling-and-return-values-723f1f07f8cd) and will look at error handling and return values.
+The fourth part of the tutorial is [here](https://medium.com/better-programming/building-the-rust-web-app-proper-error-handling-and-return-values-723f1f07f8cd) and will look at error handling and return values as well as adding some basic pagination.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod pool;
 
-use migration::MigratorTrait;
+use migration::{tests_cfg::json, MigratorTrait};
 use pool::Db;
-use rocket::{fairing::{AdHoc, self}, Rocket, Build, form::Form, serde::json::Json, http::Status, response::{Responder, self}, Request};
-use sea_orm::{ActiveModelTrait, Set, EntityTrait, QueryOrder, DeleteResult};
-use sea_orm_rocket::{Database, Connection};
+use rocket::{
+    fairing::{self, AdHoc},
+    form::Form,
+    fs::{relative, FileServer},
+    http::Status,
+    request::FlashMessage,
+    response::{self, Flash, Redirect, Responder},
+    serde::json::Json,
+    Build, Request, Rocket,
+};
+use rocket_dyn_templates::Template;
+use sea_orm::{ActiveModelTrait, EntityTrait, PaginatorTrait, QueryOrder, Set};
+use sea_orm_rocket::{Connection, Database};
 
 use entity::tasks;
 use entity::tasks::Entity as Tasks;
@@ -25,13 +36,36 @@ impl From<sea_orm::DbErr> for DatabaseError {
     }
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/?<page>&<tasks_per_page>")]
+async fn index(
+    conn: Connection<'_, Db>,
+    flash: Option<FlashMessage<'_>>,
+    page: Option<usize>,
+    tasks_per_page: Option<usize>,
+) -> Result<Template, DatabaseError> {
+    let db = conn.into_inner();
+    let page = page.unwrap_or(0);
+    let tasks_per_page = tasks_per_page.unwrap_or(5);
+
+    let paginator = Tasks::find()
+        .order_by_asc(tasks::Column::Id)
+        .paginate(db, tasks_per_page);
+    let numer_of_pages = paginator.num_pages().await?;
+    let tasks = paginator.fetch_page(page).await?;
+
+    Ok(Template::render(
+        "todo_list",
+        json!({ 
+            "tasks": tasks, 
+            "flash": flash.map(FlashMessage::into_inner),
+            "number_of_pages": numer_of_pages,
+            "current_page": page 
+        }),
+    ))
 }
 
-#[post("/addtask", data="<task_form>")]
-async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Json<tasks::Model>, DatabaseError> {
+#[post("/addtask", data = "<task_form>")]
+async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Flash<Redirect> {
     let db = conn.into_inner();
     let task = task_form.into_inner();
 
@@ -40,7 +74,14 @@ async fn add_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Re
         ..Default::default()
     };
 
-    Ok(Json(active_task.insert(db).await?))
+    match active_task.insert(db).await {
+        Ok(result) => result,
+        Err(_) => {
+            return Flash::error(Redirect::to("/"), "Issue creating the task");
+        }
+    };
+
+    Flash::success(Redirect::to("/"), "Task created!")
 }
 
 #[get("/readtasks")]
@@ -51,30 +92,51 @@ async fn read_tasks(conn: Connection<'_, Db>) -> Result<Json<Vec<tasks::Model>>,
         Tasks::find()
             .order_by_asc(tasks::Column::Id)
             .all(db)
-            .await?
+            .await?,
     ))
 }
 
-#[put("/edittask", data="<task_form>")]
-async fn edit_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Result<Json<tasks::Model>, DatabaseError> {
+#[put("/edittask", data = "<task_form>")]
+async fn edit_task(conn: Connection<'_, Db>, task_form: Form<tasks::Model>) -> Flash<Redirect> {
     let db = conn.into_inner();
     let task = task_form.into_inner();
 
-    let task_to_update = Tasks::find_by_id(task.id).one(db).await?;
+    let task_to_update = match Tasks::find_by_id(task.id).one(db).await {
+        Ok(result) => result,
+        Err(_) => {
+            return Flash::error(Redirect::to("/"), "Issue editing the task");
+        }
+    };
     let mut task_to_update: tasks::ActiveModel = task_to_update.unwrap().into();
     task_to_update.item = Set(task.item);
+    match task_to_update.update(db).await {
+        Ok(result) => result,
+        Err(_) => {
+            return Flash::error(Redirect::to("/"), "Issue editing the task");
+        }
+    };
 
-    Ok(Json(
-        task_to_update.update(db).await?
-    ))
+    Flash::success(Redirect::to("/"), "Task edited successfully!")
+}
+
+#[get("/edit/<id>")]
+async fn edit_task_page(conn: Connection<'_, Db>, id: i32) -> Result<Template, DatabaseError> {
+    let db = conn.into_inner();
+    let task = Tasks::find_by_id(id).one(db).await?.unwrap();
+
+    Ok(Template::render("edit_task_form", json!({ "task": task })))
 }
 
 #[delete("/deletetask/<id>")]
-async fn delete_task(conn: Connection<'_, Db>, id: i32) -> Result<String, DatabaseError> {
+async fn delete_task(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
     let db = conn.into_inner();
-    let result = Tasks::delete_by_id(id).exec(db).await?;
-
-    Ok(format!("{} task(s) deleted", result.rows_affected))
+    let _result = match Tasks::delete_by_id(id).exec(db).await {
+        Ok(value) => value,
+        Err(_) => {
+            return Flash::error(Redirect::to("/"), "Issue deleting the task");
+        }
+    };
+    Flash::success(Redirect::to("/"), "Task successfully deleted!")
 }
 
 async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
@@ -88,8 +150,17 @@ fn rocket() -> _ {
     rocket::build()
         .attach(Db::init())
         .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
+        .mount("/", FileServer::from(relative!("/public")))
         .mount(
-        "/",
-        routes![index, add_task, read_tasks, edit_task, delete_task],
-    )
+            "/",
+            routes![
+                index,
+                add_task,
+                read_tasks,
+                edit_task,
+                delete_task,
+                edit_task_page
+            ],
+        )
+        .attach(Template::fairing())
 }
